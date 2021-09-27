7dfd1506f4606a9e8444e976987cd2ca66fb0f68
Priyanka de Silva <cdomicolo@oswego.edu>
2021-09-27 13:38:19 +0530

    added text to indicate empty sections

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 // ADD NEW TASK
 
 const addTaskButton = document.querySelector('#submit');
+const currentTaskList = document.querySelector("#current-task-list");
+const completedTaskList = document.querySelector("#completed-task-list");
 
 addTaskButton.addEventListener('click', submitTask)
 
-// let taskId = 1;
 function submitTask(e) {
     const inputField = document.querySelector('#task-name')
     if (inputField.value !== '') {
         e.preventDefault();
         let taskName = inputField.value;
 
-        const currentTasks = document.querySelector('#current-task-list')
         const newTask = document.createElement("li")
-        // newTask.setAttribute("id", taskId);
         newTask.classList.add("task")
 
         const taskNameParagraph = document.createElement("p")
@@ -36,9 +35,14 @@ function submitTask(e) {
 
         newTask.appendChild(taskDetails)
         newTask.appendChild(deleteButton)
-        currentTasks.appendChild(newTask)
+        currentTaskList.appendChild(newTask)
 
-        // taskId++;
+        console.log(currentTaskList.childNodes)
+
+        if(currentTaskList.hasChildNodes()) {
+            document.querySelector('#no-current-tasks').style.display = 'none'
+        }
+
         inputField.value = '';
     } else {
         window.alert("Please enter Task Name");
@@ -47,41 +51,44 @@ function submitTask(e) {
 
 // MARK TASK AS COMPLETE
 
-const currentTaskList = document.querySelector("#current-task-list");
-
 currentTaskList.addEventListener("dblclick", markAsComplete)
 
 function markAsComplete(e) {
     if (e.target.classList.contains('task')) {
-        const completedTasks = document.querySelector('#completed-task-list')
-
         const taskCompleteParagraph = document.createElement("p")
         const taskCompleteDate = document.createTextNode(`Completed on: ${new Date().toLocaleString()}`)
         taskCompleteParagraph.appendChild(taskCompleteDate)
 
         e.target.firstChild.appendChild(taskCompleteParagraph)
 
-        completedTasks.appendChild(e.target)
+        completedTaskList.appendChild(e.target)
+    }
+
+    if(!currentTaskList.hasChildNodes()) {
+        document.querySelector('#no-current-tasks').style.display = 'block'
+    }
+
+    if(completedTaskList.hasChildNodes()) {
+        document.querySelector('#no-completed-tasks').style.display = 'none'
     }
 }
 
 // DELETE TASK
 
-const completedTaskList = document.querySelector("#completed-task-list");
 currentTaskList.addEventListener("click", deleteTask)
 completedTaskList.addEventListener("click", deleteTask)
+const currentTasksSection = document.querySelector("#current-tasks")
 
 function deleteTask(e) {
     if (e.target.classList.contains('delete-button')) {
         e.target.parentNode.parentNode.removeChild(e.target.parentNode)
     }
-}
 
-function isEmpty (domElement, text) {
-    if (!domElement.hasChildNodes()) {
-        domElement.innerText = text
+    if(!currentTaskList.hasChildNodes()) {
+        document.querySelector('#no-current-tasks').style.display = 'block'
+    }
+
+    if(!completedTaskList.hasChildNodes()) {
+        document.querySelector('#no-completed-tasks').style.display = 'block'
     }
 }
-
-isEmpty(currentTaskList, "No Current Tasks to Display");
-isEmpty(completedTaskList, "No Completed Tasks to Display");
